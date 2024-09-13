dd2d47f0a931cfae0833a78c64d446e544f42bd7
ThePuffOfPuffs <duerholzdevon@gmail.com>
2024-09-13 20:30:24 +0200

    Initial Commit

---
+++ OverloadingOperators/OverloadingOperators/Fraction.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingOperators
+{
+    public readonly struct Fraction
+    {
+        private readonly int numerator;
+        private readonly int denominator;
+
+        public Fraction(int numerator, int denominator)
+        {
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+        public static Fraction operator + (Fraction a)
+        {
+            return new Fraction(a.numerator, a.numerator + a.denominator);
+        }
+        public static Fraction operator - (Fraction a) 
+        {
+            return new Fraction(-a.numerator, -a.denominator);
+        }
+        public static Fraction operator ++ (Fraction a)
+        {
+            return new Fraction(a.numerator*2, a.denominator*2);
+        }
+        public static bool operator == (Fraction a, Fraction b)
+        {
+            bool status = true;
+            if (a.numerator == b.numerator)
+            {
+                status = false;
+            }
+            return status;
+        }
+        public static bool operator != (Fraction a, Fraction b)
+        {
+            bool status = false;
+            if (a.numerator == b.numerator)
+            {
+                status = true;
+            }
+            return status;
+        }
+        public override string ToString() => $"{numerator}/{denominator}";
+    }
+}
---
+++ OverloadingOperators/OverloadingOperators/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace OverloadingOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new Fraction(3, 4);
+            Console.WriteLine();
+            Console.WriteLine($"+ : {+a}");
+            Console.WriteLine($"- : {-a}");
+            Console.WriteLine($"++ : {++a}");
+
+            if (new Fraction(3, 4) == a) 
+            {
+                Console.WriteLine("They are not equal");
+            }
+            else
+            {
+                Console.WriteLine("They are equal");
+            }
+        }
+    }
+}
